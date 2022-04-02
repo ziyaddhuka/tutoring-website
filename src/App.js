f0ca3925e102c0720eddab7c75e2ddf3888c0994
ziyaddhuka <ziyaddhuka7@gmail.com>
2022-04-02 17:33:13 -0500

    Updated folder paths and moved files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Footer from './Footer';
+import Footer from './components/Footer';
 import Header from './components/header';
 
 function App() {
   return (
   <div>
-    <div><Header/> </div>
+    <div>
+      <Header/> 
+    </div>
     <Footer/>
   </div>
     
