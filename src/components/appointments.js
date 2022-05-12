dd501e89a787cd3f4384d0b793e37f5e44b6c59b
Sameeerr11 <Sameeerr11@gmail.com>
2022-05-12 00:43:39 -0500

    Cancel appointment

--- src/components/appointments.js
+++ src/components/appointments.js
@@ -4,6 +4,16 @@ function Appointments(){
     const [apptsData, setApptsData] = useState([]);
     const [error, setError] = useState(null);
 
+  function cancelAppointment(ex){    
+    fetch(`http://localhost:3000/api/appts/${ex}`, { method: 'DELETE'})
+    .then((response) => {
+        if(!response.ok) throw new Error(response.status);
+        else {
+            window.location.reload(true);
+        };
+      })
+  }
+
     useEffect( () => {
         fetch('http://localhost:3000/api/appts', {
           headers: {
@@ -20,7 +30,7 @@ function Appointments(){
           setError(error);
         })
     }, []);
-    console.log(apptsData);
+    // console.log(apptsData);
 
     const dat = apptsData
     .map((each_data)=>{
@@ -30,8 +40,10 @@ function Appointments(){
             <p class="mx-3 my-1"><b>Course:</b> {each_data.course_name}</p>
             <p class="mx-3 my-1"><b>Tutor:</b> {each_data.tutor_name}</p>
             <p class="mx-3 my-1"><b>Student:</b> {each_data.student_name}</p>
-            <button className = "btn btn-warning mx-3 my-1" id="cancel-appt-button">Cancel</button>
-            <button className = "btn btn-success mx-3 my-1" id="update-appt-button">Update</button>
+            <a href='/'>
+              <button className = "btn btn-success mx-3 my-1" id="update-appt-button">Cancel</button>
+            </a>
+            <button className = "btn btn-warning mx-3 my-1" id="cancel-appt-button" onClick={() => cancelAppointment(each_data._id)}>Delete</button>
             <hr/>
         </div>
     });
